780d88bf638f1b4eb0e74bb5374f723305a3c4dd
pszmk <przemyslawpietrzak2001@gmail.com>
2022-07-21 09:23:14 +0200

    Develop setup.py.

---
+++ eaftbt-venv/bin/eaftbt_script.py
@@ -0,0 +1,7 @@
+#!/home/pszmk/coding/repos/Exam_Automation_for_test_based_teaching/eaftbt-venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'eaftbt==1.0','eaftbt_script.py'
+__requires__ = 'eaftbt==1.0'
+__import__('pkg_resources').require('eaftbt==1.0')
+__file__ = '/home/pszmk/coding/repos/Exam_Automation_for_test_based_teaching/eaftbt/bin/eaftbt_script.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 from setuptools import setup
+
+setup(name='eaftbt',
+	version='1.0',
+	# list folders, not files
+	packages=['eaftbt',
+		'eaftbt.test'],
+	scripts=['eaftbt/bin/eaftbt_script.py'],
+	package_data={'eaftbt':['data/eaftbt_data.txt']}
+	)
