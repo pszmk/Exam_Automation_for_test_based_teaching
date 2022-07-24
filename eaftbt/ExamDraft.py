40c0b7041af72c3b83eb5160e78ca926b11a0cad
pszmk <przemyslawpietrzak2001@gmail.com>
2022-07-24 20:17:22 +0200

    Fix malfunctioning general Question class with its temporary filesytem.

--- eaftbt/ExamDraft.py
+++ eaftbt/ExamDraft.py
@@ -1,52 +1,61 @@
 from abc import ABC, abstractmethod
 
+class NoQuestionIdError(BaseException):
+    pass
 
 class Question(ABC):
     '''
-    Abstract class with general methods.
+    Abstract class handling questions with general methods.
     '''
-    default_substitution = {"id": None, "substitution": []}
+    default_substitution = {"substitution_id": None, "substitution": []}
 
     def __init__(self, question_id):
-        # self.questions_path = questions_path
         self.question_id = question_id
-        self.check_question_id()
+        self.is_question_id = self.check_question_id()
+        if not self.is_question_id:
+            raise NoQuestionIdError("Provided question_id is not valid.")
         self.substitution = self.default_substitution.copy()
 
     def check_question_id(self):
         import json
         from pathlib import Path
         try:
-            with open(Path(__file__).parent / "data/questions-data" / "questions-data.json") as qdata_json:
+            with open(Path(__file__).parent / "data" / "questions-data" / "questions-data.json") as qsdata_json:
+                qsdata = json.load(qsdata_json)
+                return self.question_id in qsdata["question_id_list"]
+        except FileNotFoundError(f'Cannot find folder with data related with question: {self.question_id}.') as e:
+            print(str(e.value))
+            # print(f'Cannot find folder with data related with question: {self.question_id}.')
+
+    def check_substitution_id(self, substitution_id):
+        if not self.is_question_id:
+            raise NoQuestionIdError("There is no question_id.")
+        import json
+        from pathlib import Path
+        try:
+            with open(Path(__file__).parent / "data/questions-data" / f"{self.question_id}" / f"qdata-{self.question_id}.json") as qdata_json:
                 qdata = json.load(qdata_json)
-                if not self.question_id in qdata["question_id_list"]:
-                    # raise error
-                    print('check_question_id wrong id of the question')
-        except:
-            # raise error
-            print('check_question_id oops')
+                return substitution_id in qdata["substitution_id_list"]
+        except FileNotFoundError(f'Cannot find folder with data related with question: {self.question_id}.') as e:
+            print(str(e.value))
 
     def set_substitution_by_id(self, substitution_id):
         '''
         Set the substitution by reference to its unique id.
         '''
-        # if substitution_id == "" or not self.check_substitution_id():
-            # raise error
-            # pass
+        if not self.is_question_id:
+            raise NoQuestionIdError("There is no question_id.")
+        if not self.check_substitution_id(substitution_id):
+            raise ValueError("Wrong substitution id!")
+
+        self.substitution["substitution_id"] = substitution_id
         from pathlib import Path
         try:
-            self.substitution["id"] = substitution_id
-            with open(
-                    Path(
-                        __file__).parent / "data/questions-data" / self.question_id / f'latex-{self.substitution["id"]}-{self.question_id}.txt') as latex_substitution:
-                self.substitution["substitution"] = [line.strip() for line in latex_substitution if
-                                                     not line.startswith('#')]
-        except:
+            with open(Path(__file__).parent / "data" / "questions-data" / self.question_id / f'latex-{self.substitution["substitution_id"]}-{self.question_id}.txt') as latex_substitution:
+                self.substitution["substitution"] = [line.strip() for line in latex_substitution if not line.startswith('#')]
+        except FileNotFoundError("File with substitution not found. Substitution was reset.") as e:
+            print(str(e.value))
             self.reset_substitution()
-            # raise error
-            # tutaj ma byc error ze nie ma sciezki
-            print(["szkoda"])
-            pass
 
     def get_substitution(self):
         return self.substitution
@@ -63,15 +72,13 @@ class Question(ABC):
 
 
 class EmptyQuestion(Question):
-    question_id = None
+    # question_id = None
 
     def set_substitution_manually(self):
         pass
 
 
-class TestQuestion(Question):
-    question_id = None
-
+class TryOutQuestion(Question):
     def set_substitution_manually(self):
         pass
 
@@ -94,14 +101,46 @@ if __name__ == '__main__':
     '''
     Pre-testing EmptyQuestion 
     '''
-    question_id = "1gg5l35hj3"
-    r = EmptyQuestion(question_id)
-    substitution_id = "000"
-    r.set_substitution_by_id('000')
-    print(r.get_substitution())
-    print(r.question_id)
-    r.reset_substitution()
-    print(r.get_substitution())
+    tq = TryOutQuestion("1gg5l35hj3")
+    # tq.set_substitution_by_id('000')
+
+    from pathlib import Path
+    substitution = {"substitution_id": "111", "substitution":[]}
+    try:
+        with open(Path(__file__).parent / "data" / "questions-data" / "1gg5l35hj3" / f'latex-{substitution["substitution_id"]}-{"1gg5l35hj3"}.txt') as latex_substitution:
+            substitution["substitution"] = [line.strip() for line in latex_substitution if
+                                                 not line.startswith('#')]
+    except FileNotFoundError("File with substitution not found. Substitution was reset.") as e:
+        print(str(e.value))
+        # self.reset_substitution()
+
+    # print(NoQuestionIdError('Cannot find folder with data related with question:.').type())
+    # print(Exception('Cannot find folder with data related with question:.').type())
+    # try:
+    #     1/0
+    # except NoQuestionIdError('Cannot find folder with data related with question:.') as e:
+    #     print(str(e.value))
+
+    # question_id = "1gg5l35hj3"
+    # r = EmptyQuestion(question_id)
+    # substitution_id = "000"
+    # r.set_substitution_by_id('000')
+    # print(r.get_substitution())
+    # print(r.question_id)
+    # r.reset_substitution()
+    # print(r.get_substitution())
+    # tq = TestQuestion("")
+    # tq = TestQuestion(question_id)
+    # import json
+    # from pathlib import Path
+    #
+    # try:
+    #     with open(Path(__file__).parent / "data/questions-data" / "questions-data.json") as qdata_json:
+    #         qdata = json.load(qdata_json)
+    #         if question_id in qdata["question_id_list"]:
+    #             print("question_id in")
+    # except FileNotFoundError:
+    #     pass
 # from os import path
 # print(path.join('eaftbt','data','1gg5l35hj3','tmp.txt'))
 # with open(path.join('eaftbt','data','1gg5l35hj3','tmp.txt')) as tmp:
--- eaftbt/test_ExamDraft.py
+++ eaftbt/test_ExamDraft.py
@@ -1,14 +1,16 @@
-from eaftbt.ExamDraft import ExamDraft, TestQuestion
+import pytest
+from eaftbt.ExamDraft import ExamDraft, TryOutQuestion, NoQuestionIdError
 
+question_id_test_1 = "1gg5l35hj3"
 
-def test_exam_draft_basic():
-    exd = ExamDraft()
-    var0 = exd.get_variant(0)
-    var1 = exd.get_variant(101)
-    var2 = exd.get_variant(69420)
-    assert len(var0) == 0
-    assert len(var1) == 0
-    assert len(var2) == 0
+# def test_exam_draft_basic():
+#     exd = ExamDraft()
+#     var0 = exd.get_variant(0)
+#     var1 = exd.get_variant(101)
+#     var2 = exd.get_variant(69420)
+#     assert len(var0) == 0
+#     assert len(var1) == 0
+#     assert len(var2) == 0
 
 
 # def test_add_question():
@@ -19,55 +21,82 @@ def test_exam_draft_basic():
 #     assert len(var) == 0
 #     assert var[0] is que
 
-def test_basic_question():
-    default_substitution = {"id": None, "substitution": []}
+def test_question_basic():
+    default_substitution = {"substitution_id": None, "substitution": []}
 
-    q = TestQuestion("")
-    assert q.question_id == ""
-    assert q.substitution == default_substitution
+    with pytest.raises(NoQuestionIdError) as e:
+        tq = TryOutQuestion("")
+    assert str(e.value) == "Provided question_id is not valid."
+    assert not e.type == ValueError
+    assert e.type == NoQuestionIdError
 
-    q.set_substitution_manually()
-    assert q.question_id == ""
-    assert q.substitution == default_substitution
+    with pytest.raises(NoQuestionIdError) as e:
+        tq = TryOutQuestion("5783783721rr")
+    assert str(e.value) == "Provided question_id is not valid."
+    assert not e.type == ValueError
+    assert e.type == NoQuestionIdError
+
+    tq = TryOutQuestion(question_id_test_1)
+    tq.set_substitution_manually()
+    assert not tq.question_id == ""
+    assert tq.substitution == default_substitution
+
+def test_check_substitution_id():
+    tq = TryOutQuestion(question_id_test_1)
+    tq.question_id = ""
+    tq.is_question_id = False
+    with pytest.raises(NoQuestionIdError) as e:
+        tq.check_substitution_id("")
+    assert str(e.value) == "There is no question_id."
+    assert e.type == NoQuestionIdError
+
+    tq = TryOutQuestion(question_id_test_1)
+    assert not tq.check_substitution_id("")
+    assert not tq.check_substitution_id('00')
+    assert not tq.check_substitution_id('uut')
+    assert tq.check_substitution_id('000')
+
+    tq.reset_substitution()
+    assert tq.check_substitution_id('000')
+
 
 def test_set_substitution_by_id():
-    default_substitution = {"id": None, "substitution": []}
+    default_substitution = {"substitution_id": None, "substitution": []}
+    substitution_000_question_1gg5l35hj3 = {"substitution_id" : "000", "substitution" : ['\\frac{2}{5}\\frac{3}{7}', '4']}
 
-    q = TestQuestion("")
-    q.set_substitution_by_id('111')
-    assert q.question_id == ""
-    assert q.substitution == default_substitution
+    tq = TryOutQuestion(question_id_test_1)
+    tq.is_question_id = False
+    with pytest.raises(NoQuestionIdError) as e:
+        tq.set_substitution_by_id('000')
+    assert str(e.value) == "There is no question_id."
+    assert e.type == NoQuestionIdError
 
-    q.set_substitution_by_id('111')
-    assert q.question_id == ""
-    assert q.substitution == default_substitution
+    tq = TryOutQuestion(question_id_test_1)
+    with pytest.raises(ValueError) as e:
+        tq.set_substitution_by_id('111')
+    assert str(e.value) == "Wrong substitution id!"
+    assert e.type == ValueError
+    assert tq.substitution == default_substitution
 
-    q.set_substitution_by_id('000')
-    assert q.question_id == ""
-    assert q.substitution == default_substitution
+    tq = TryOutQuestion(question_id_test_1)
+    tq.set_substitution_by_id('000')
+    assert tq.substitution == substitution_000_question_1gg5l35hj3
 
-    q = TestQuestion("1gg5l35hj3")
-    assert q.question_id != ""
-    assert q.substitution == default_substitution
-
-    q.set_substitution_by_id('111')
-    assert q.substitution == default_substitution
-
-    new_substitution = {"id": "000", "substitution": ["\\frac{2}{5}\\frac{3}{7}", "4"]}
-
-    q.set_substitution_by_id('000')
-    assert q.substitution != default_substitution
-    assert q.substitution["id"] == '000'
-    assert q.substitution == new_substitution
-
-def test_question_get_substitution():
-    q = TestQuestion("")
-    assert q.get_substitution() == new_substitution
-
-    q.set_substitution_by_id("111")
-    assert q.get_substitution() == default_substitution
-
+    tq = TryOutQuestion(question_id_test_1)
+    assert tq.question_id != ""
+    assert tq.substitution == default_substitution
 
 
 def test_question_get_substitution():
-    pass
+    default_substitution = {"substitution_id": None, "substitution": []}
+    new_substitution = {"substitution_id": "000", "substitution": ["\\frac{2}{5}\\frac{3}{7}", "4"]}
+    tq = TryOutQuestion(question_id_test_1)
+    assert tq.get_substitution() == default_substitution
+
+    tq.set_substitution_by_id("000")
+    assert tq.get_substitution() == new_substitution
+
+    tq.reset_substitution()
+    assert tq.get_substitution() == default_substitution
+
+
